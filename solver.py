7ab6a15b7ceaf2c39818c3997ac528d287b3ebcc
FelipeCord <felipemachadocordeiro@gmail.com>
2018-09-20 19:10:34 -0300

    Create input array

--- solver.py
+++ solver.py
@@ -13,10 +13,6 @@ def Pivot(A , i, j):
         if(A[row,j] != 0 and row != i):
             E[row,i] = -(A[row,j]/A[i,j])
     A = removeSmallNumbers(E.dot(A))
-    print(A)
-    print("\n")
-    print(E)
-    print("\n\n")
     return A
 
 def Canon( A ):
@@ -31,8 +27,76 @@ def Canon( A ):
                 break
     return A
 
-I = np.identity(4)
-A = np.arange(20).reshape(4, 5) ** 2
-print(A)
-print("\n\n")
-Canon(A)
+def CreateRestrictions(InputFile, VariableNumber, RestrictionNumber, FreeVariables, Objective):
+    Restrictions = []
+    B = []
+    FPIVariables = 0
+    for i in range(RestrictionNumber):
+        Line = InputFile.readline().split()
+
+        Restriction = [0]
+        for Variable in range(VariableNumber):
+            if(Variable in FreeVariables):
+                Restriction.extend([float(Line[Variable] ),-float(Line[Variable])])
+            else:
+                Restriction.append(float(Line[Variable]))
+
+        for Variable in range(FPIVariables):
+            Restriction.append(0)
+        
+        if(Line[VariableNumber] == ">="):
+            Restriction.append(-1)
+            Objective.append(0)
+            FPIVariables += 1
+            for Array in Restrictions:
+                Array.append(0)
+        elif(Line[VariableNumber] == "<="):
+            Restriction.append(1)
+            FPIVariables += 1
+            Objective.append(0)
+            for Array in Restrictions:
+                Array.append(0)
+
+        Restrictions.append(Restriction)
+        B.append(float(Line[-1]))
+        
+    for Index,Restriction in enumerate(Restrictions):
+        Restriction.append(B[Index])
+
+    return Restrictions
+        
+
+def CreateInputArray(file):
+    
+    InputFile = open(file,'r')
+    VariableNumber = int(InputFile.readline())
+    RestrictionNumber = int(InputFile.readline())
+    
+    FreeVariables = []
+    Line = InputFile.readline().split()
+    for Variable,Value in enumerate(Line):
+        if(int(Value) != 1):
+            FreeVariables.append(Variable)
+    
+    Objective = [1]
+    Line = InputFile.readline().split()
+    for Variable,Value in enumerate(Line):
+        if(Variable in FreeVariables):
+            Objective.extend([float(Value),-float(Value)])
+        else:
+            Objective.append(float(Value))
+    Restrictions = CreateRestrictions(InputFile, VariableNumber, RestrictionNumber, FreeVariables, Objective)
+    Objective.append(0)
+    A = [Objective]
+    A.extend(Restrictions)
+    for Line in A:
+        print(Line)
+    # print(A)
+    return A
+
+CreateInputArray('exercicio9a.txt')
+# I = np.identity(4)
+# A = np.arange(20).reshape(4, 5) ** 2
+# print(A)
+# print(A[1,range(1,A.shape[1] )])
+# Canon(A)
