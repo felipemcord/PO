06d175ef86414a3fbf177a9cd91dac0d08209b9c
FelipeCord <felipemachadocordeiro@gmail.com>
2018-09-20 21:09:50 -0300

    Create class and conversion to numpy

--- solver.py
+++ solver.py
@@ -1,100 +1,149 @@
 import numpy as np
+import sys
+
+class LP:
+
+    VariableNumber = 0
+    RestrictionNumber = 0
+    FPIVariables = 0
+    E = []
+    pivotLines = []
+    Objecvtive = []
+    Restrictions = []
+    FreeVariables = []
+    A = []
+    B = []
+
+class LPNP:
+    def __init__(self,PL):
+        self.VariableNumber = PL.VariableNumber
+        self.RestrictionNumber = PL.RestrictionNumber
+        self.FPIVariables = PL.FPIVariables
+        self.A = np.array(PL.A)
+        self.E = np.identity(self.A.shape[0])
+        self.Objecvtive = np.array(PL.Objective)
+        self.Restrictions = np.array(PL.Restrictions)
+        self.B = np.array(PL.B)
+        self.pivotLines = []
+
+    VariableNumber = 0
+    RestrictionNumber = 0
+    FPIVariables = 0
+    E = []
+    pivotLines = []
+    Objecvtive = []
+    Restrictions = []
+    FreeVariables = []
+    A = []
+    B = []
 
 def removeSmallNumbers(A):
     A[abs(A) < 10**(-10)] = 0
     return A
 
-def Pivot(A , i, j):
-    E = np.identity(A.shape[0])
+def Pivot(PL , i, j):
+    E = np.identity(PL.A.shape[0])
     print(i,j)
-    if(A[i,j] != 1):
-        E[i,...] /= A[i,j]
-    for row in range(A.shape[0]):
-        if(A[row,j] != 0 and row != i):
-            E[row,i] = -(A[row,j]/A[i,j])
-    A = removeSmallNumbers(E.dot(A))
-    return A
 
-def Canon( A ):
-    firstLine = A[0]
-    pivotLines = []
+    if(PL.A[i,j] != 1):
+        E[i,...] /= PL.A[i,j]
+    
+    for row in range(PL.A.shape[0]):
+        if(PL.A[row,j] != 0 and row != i):
+            E[row,i] = -(PL.A[row,j]/ PL.A[i,j] )
+    PL.E = E.dot(PL.E)
+    PL.A = removeSmallNumbers(E.dot(PL.A))
+    return PL
+
+def Canon( PL ):
+    firstLine = PL.A[0]
+    PL.pivotLines = []
     for i in list(reversed(range(firstLine.shape[0] - 1))):
-        column = A[...,i]
+        column = PL.A[...,i]
         for j in range(1,column.shape[0]):
-            if(column[j] != 0 and j not in pivotLines):
-                A = Pivot(A,j,i)
-                pivotLines.append(j)
+            if(column[j] != 0 and j not in PL.pivotLines):
+                PL = Pivot(PL,j,i)
+                PL.pivotLines.append(j)
                 break
-    return A
+    return PL
 
-def CreateRestrictions(InputFile, VariableNumber, RestrictionNumber, FreeVariables, Objective):
-    Restrictions = []
-    B = []
-    FPIVariables = 0
-    for i in range(RestrictionNumber):
+def CreateRestrictions(InputFile, PL):
+    PL.Restrictions = []
+    PL.B = []
+    PL.FPIVariables = 0
+    for i in range(PL.RestrictionNumber):
         Line = InputFile.readline().split()
 
         Restriction = [0]
-        for Variable in range(VariableNumber):
-            if(Variable in FreeVariables):
+        for Variable in range(PL.VariableNumber):
+            if(Variable in PL.FreeVariables):
                 Restriction.extend([float(Line[Variable] ),-float(Line[Variable])])
             else:
                 Restriction.append(float(Line[Variable]))
 
-        for Variable in range(FPIVariables):
+        for Variable in range(PL.FPIVariables):
             Restriction.append(0)
         
-        if(Line[VariableNumber] == ">="):
+        if(Line[PL.VariableNumber] == ">="):
             Restriction.append(-1)
-            Objective.append(0)
-            FPIVariables += 1
-            for Array in Restrictions:
+            PL.Objective.append(0)
+            PL.FPIVariables += 1
+            for Array in PL.Restrictions:
                 Array.append(0)
-        elif(Line[VariableNumber] == "<="):
+        elif(Line[PL.VariableNumber] == "<="):
             Restriction.append(1)
-            FPIVariables += 1
-            Objective.append(0)
-            for Array in Restrictions:
+            PL.FPIVariables += 1
+            PL.Objective.append(0)
+            for Array in PL.Restrictions:
                 Array.append(0)
 
-        Restrictions.append(Restriction)
-        B.append(float(Line[-1]))
-        
-    for Index,Restriction in enumerate(Restrictions):
-        Restriction.append(B[Index])
+        PL.Restrictions.append(Restriction)
+        PL.B.append(float(Line[-1]))
 
-    return Restrictions
+    for Index,Restriction in enumerate(PL.Restrictions):
+        Restriction.append(PL.B[Index])
+
+    return PL.Restrictions
         
 
 def CreateInputArray(file):
-    
+    PL = LP()
     InputFile = open(file,'r')
-    VariableNumber = int(InputFile.readline())
-    RestrictionNumber = int(InputFile.readline())
+    PL.VariableNumber = int(InputFile.readline())
+    PL.RestrictionNumber = int(InputFile.readline())
     
-    FreeVariables = []
+    PL.FreeVariables = []
     Line = InputFile.readline().split()
     for Variable,Value in enumerate(Line):
         if(int(Value) != 1):
-            FreeVariables.append(Variable)
+            PL.FreeVariables.append(Variable)
     
-    Objective = [1]
+    PL.Objective = [1]
     Line = InputFile.readline().split()
     for Variable,Value in enumerate(Line):
-        if(Variable in FreeVariables):
-            Objective.extend([float(Value),-float(Value)])
+        if(Variable in PL.FreeVariables):
+            PL.Objective.extend([float(Value),-float(Value)])
         else:
-            Objective.append(float(Value))
-    Restrictions = CreateRestrictions(InputFile, VariableNumber, RestrictionNumber, FreeVariables, Objective)
-    Objective.append(0)
-    A = [Objective]
-    A.extend(Restrictions)
-    for Line in A:
+            PL.Objective.append(float(Value))
+    # PL.Restrictions = CreateRestrictions(InputFile, VariableNumber, RestrictionNumber, FreeVariables, Objective)
+    PL.Restrictions = CreateRestrictions(InputFile,PL)
+    PL.Objective.append(0)
+    PL.A = [PL.Objective]
+    PL.A.extend(PL.Restrictions)
+    for Line in PL.A:
         print(Line)
     # print(A)
-    return A
+    return PL
 
-CreateInputArray('exercicio9a.txt')
+
+print(sys.argv)
+PL =  CreateInputArray(sys.argv[1])
+PLNP = LPNP(PL)
+np.set_printoptions(linewidth=200,precision=2,suppress=True)
+print(PLNP.A)
+Canon(PLNP)
+print(PLNP.A)
+print(PLNP.A[1,0] == 0)
 # I = np.identity(4)
 # A = np.arange(20).reshape(4, 5) ** 2
 # print(A)
